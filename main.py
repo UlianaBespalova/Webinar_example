538604b45d7e33ab45e85523b3662983ff06dbdc
Bespalova Uliana <u.bespalova@mail.msk>
2023-11-26 10:20:28 +0300

    Добавила строчку в файл main

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ filters = ["Черно-белый"]
 
 
 print('Добрый день! Выберите фильтр:')
+print('Введите число')
 
 for i in range(len(filters)): # Показываем меню
     print(f"{i+1}. {filters[i]}")
@@ -11,10 +12,4 @@ print(f"{len(filters)+1}. Выход")
 num = int(input()) # Пользователь вводит норер
 our_filter = filters[num - 1] # Берём нужный фильтр
 print(f"Вы выбрали фильтр {our_filter}") # Печатаем описание
-
-print('Программа успешно завершена!')
-
-
-
-
 
