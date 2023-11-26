f0b323305a2a6d6755ad1aef2238ec2b0a5a9028
Bespalova Uliana <u.bespalova@mail.msk>
2023-11-26 10:18:15 +0300

    Добавила первую версию кода

---
+++ main.py
@@ -0,0 +1,20 @@
+
+filters = ["Черно-белый"]
+
+
+print('Добрый день! Выберите фильтр:')
+
+for i in range(len(filters)): # Показываем меню
+    print(f"{i+1}. {filters[i]}")
+print(f"{len(filters)+1}. Выход")
+
+num = int(input()) # Пользователь вводит норер
+our_filter = filters[num - 1] # Берём нужный фильтр
+print(f"Вы выбрали фильтр {our_filter}") # Печатаем описание
+
+print('Программа успешно завершена!')
+
+
+
+
+
